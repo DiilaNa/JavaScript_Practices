2286336cba934491c415ddc917bfa246cb47a047
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-29 15:59:28 +0530

    js getElemntsbyID AND NAME

---
+++ JavaScript/DoM.js
@@ -0,0 +1,9 @@
+var h1_elements = document.getElementsByTagName("h1")
+console.log(h1_elements[0])
+console.log(h1_elements[1])
+
+h1_elements[0].innerText="HELLO IJSE"
+console.log(h1_elements[0].innerHTML)
+
+console.log(document.getElementById("id1"))
+
