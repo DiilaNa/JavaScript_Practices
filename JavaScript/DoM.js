1739472fb41710f7e36714eaba5d833e3b5db1fa
DiilaNa <liyanaarachchidilan@gmail.com>
2025-04-05 09:11:05 +0530

    add

--- JavaScript/DoM.js
+++ JavaScript/DoM.js
@@ -5,5 +5,18 @@ console.log(h1_elements[1])
 h1_elements[0].innerText="HELLO IJSE"
 console.log(h1_elements[0].innerHTML)
 
-console.log(document.getElementById("id1"))
+var getSubjectID = document.getElementById("id1")
+console.log(getSubjectID[0])
+
+var getSubjectBYName = document.getElementsByClassName("id")
+console.log(getSubjectBYName[0])
+
+let myNames = document.getElementsByName("myname")
+console.log(myNames[0])
+console.log(myNames[0].innerText)
+console.log(myNames[0].innerHTML)
+myNames[0].innerText = "TYPESCRIPT"
+
+var queryLIST = document.querySelector("h6>p")
+console.log(document.querySelector("h6>p"))
 
