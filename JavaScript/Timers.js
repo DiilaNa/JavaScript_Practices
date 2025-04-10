3eb28973b3170f56bab7621ef6d4a9144f6377c7
DiilaNa <liyanaarachchidilan@gmail.com>
2025-04-10 12:16:46 +0530

    JS TIMERS - Srt Timeout

---
+++ JavaScript/Timers.js
@@ -0,0 +1,7 @@
+const function1 = () => {
+    console.log('hello')
+}
+console.log('HI')
+
+/*-----SET TIMEOUT----Delay the time and call the function-----*/
+setTimeout(function1,10000);/*10000ms*/
