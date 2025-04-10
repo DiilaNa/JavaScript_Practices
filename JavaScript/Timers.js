d76ad53f30ea10c0502c7253f2b772c25d91a165
DiilaNa <liyanaarachchidilan@gmail.com>
2025-04-10 14:17:00 +0530

    JS TIMERS - Srt Interval

--- JavaScript/Timers.js
+++ JavaScript/Timers.js
@@ -1,7 +1,13 @@
 const function1 = () => {
     console.log('hello')
 }
-console.log('HI')
+
+const function2 = () => {
+    console.log('HI')
+}
 
 /*-----SET TIMEOUT----Delay the time and call the function-----*/
 setTimeout(function1,10000);/*10000ms*/
+
+/*------SET INTERVAL----Delay the time and call the function and recursively does this-------*/
+setInterval(function2,10000)
