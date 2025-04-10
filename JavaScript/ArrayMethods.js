4a1c9f5216e9a9d075f4f541d29e0d4a12b68b66
DiilaNa <liyanaarachchidilan@gmail.com>
2025-04-10 09:33:20 +0530

    Array Methods-Shift()/comments updated

--- JavaScript/ArrayMethods.js
+++ JavaScript/ArrayMethods.js
@@ -1,7 +1,18 @@
 
-/*----------Array Methods in JS-----PUSH()--------//////*/
-let myArray = ['A','B','C'];
+/*----------Array Methods in JS-------------//////*/
+
+/*------PUSH()-Adding an element to the end of an array-------*/
+let myArray = ['A','B','C','E'];
+console.log(myArray);
+//adding extra items to array
+myArray.push('F')
 console.log(myArray);
 
-myArray.push('D')
-console.log(myArray);
+/*------POP()-Remove the last element in the array-----*/
+console.log(myArray.pop())
+
+
+/*------Shift()-Remove the first Element--------*/
+let myArray2 = ['anil','Ranil','Nimal'];
+console.log(myArray2.shift())
+console.log(myArray2)
