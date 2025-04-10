6d3cf9c9958215e021874d790dfdd9edafed6940
DiilaNa <liyanaarachchidilan@gmail.com>
2025-04-10 10:17:28 +0530

    Array Methods-SLICE() and uses

--- JavaScript/ArrayMethods.js
+++ JavaScript/ArrayMethods.js
@@ -25,4 +25,28 @@ console.log(myArray2)
 let myArray3 = myArray.concat(myArray2)
 console.log(myArray3) /*---myArray+myArray2----*/
 
+/*------Slice()- update a value without modifying the original. Create a shadow copy and Update --------*/
+
+let x = [1,2,3,4,5]
+let y = x;
+y[0]=299;
+console.log(x)
+console.log(y) /*Here it updates the x array value too,both x and y values change*/
+let z = x.slice();
+z[0]=1000;
+console.log(z)
+console.log(x)
+
+/*Get the shadow copy of the array from the mention element without changing the original*/
+let z2 = x.slice(2)
+console.log(z2)
+/*get the range of the values of the shadow copy without changing the original array*/
+let a = [10,20,30,40,50,60,70,80,90,100]
+let z3 = a.slice(2,6)
+console.log(z3)
+
+/*[-1 means the last element]count from the last element to first,
+* get a shadow copy and get the values from the -3 element without changing the original values of the array */
+let z4 = a.slice(-3)
+console.log(z4)
 
